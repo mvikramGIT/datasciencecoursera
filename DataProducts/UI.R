45b21a70e4ce1ba6a057c2b914a3397af28df76a
Vikram Mukundan <Vikram@vikram-mukundans-macbook-pro-2.local>
2015-01-25 09:49:09 -0800

    Updates UI.R with documentation

--- DataProducts/UI.R
+++ DataProducts/UI.R
@@ -7,6 +7,10 @@ shinyUI(pageWithSidebar(
     numericInput('sigma','Guess ensemble sd', value = 0.2, min = 0.1, max = 10)
     ),
   mainPanel(
-    plotOutput('newHist')
+    plotOutput('newHist'),
+    h6('This app simulates the central limit theorem by generating the ensemble mean of 1000
+       exponential samples with lambda = 0.2 (mean = 5). The size of each sample is set by the
+       user as n, and the output simulates the mean and standard deviation of the ensemle mean.
+       The user can also guess the esemble mean from the data. Hint: sigma/sqrt(n)!')
   )
 ))
